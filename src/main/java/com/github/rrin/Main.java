13a23a5e5e8a86f9d14a8a6c05921e0d2ab38ede
r-rin <rafikov.rinat.m@gmail.com>
2025-06-15 20:34:02 +0300

    implement udp based client

--- src/main/java/com/github/rrin/Main.java
+++ src/main/java/com/github/rrin/Main.java
@@ -1,18 +1,15 @@
 package com.github.rrin;
 
-import com.github.rrin.implementation.udp.UdpReceiver;
-import com.github.rrin.util.MockClientUdp;
-
 public class Main {
     public static void main(String[] args) {
         String serverHost = "127.0.0.1";
         int serverPort = 5555;
 
         StoreServerUDP app = new StoreServerUDP(serverPort);
-        MockClientUdp client = new MockClientUdp(serverHost, serverPort);
+        StoreClientUDP client = new StoreClientUDP(serverHost, serverPort);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("\n!!! Stopping test client !!!");
+            System.out.println("\n!!! Stopping everything !!!");
             app.stop();
             client.stop();
         }));
@@ -21,12 +18,18 @@ public class Main {
             app.start();
             client.start();
 
+            client.addGoods("Laptop", 15);
+            client.setPrice("Laptop", 1199.9);
+            client.queryQuantity("Laptop");
+
             while (client.isRunning()) {
                 Thread.sleep(1000);
             }
 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         } finally {
             client.stop();
         }
