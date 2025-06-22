3b3c714c19eff334d0c10b5045ac4bc1ac18aca3
r-rin <rafikov.rinat.m@gmail.com>
2025-06-22 19:04:41 +0300

    records for Product and Group

---
+++ src/main/java/com/github/rrin/dto/Group.java
@@ -0,0 +1,4 @@
+package com.github.rrin.dto;
+
+public record Group(int id, String name) {
+}
---
+++ src/main/java/com/github/rrin/dto/Product.java
@@ -0,0 +1,4 @@
+package com.github.rrin.dto;
+
+public record Product(int id, String name, int price, int quantity) {
+}
